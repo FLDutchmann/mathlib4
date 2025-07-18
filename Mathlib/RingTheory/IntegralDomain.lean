4e872058089a956a703efaf07ea0e1d95969fb5b
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2025-07-18 13:23:58 +0100

    reduce diff

--- Mathlib/RingTheory/IntegralDomain.lean
+++ Mathlib/RingTheory/IntegralDomain.lean
@@ -163,7 +163,7 @@ theorem div_eq_quo_add_rem_div (f : R[X]) {g : R[X]} (hg : g.Monic) :
   · have hg' : algebraMap R[X] K g ≠ 0 :=
       -- Porting note: the proof was `by exact_mod_cast Monic.ne_zero hg`
       (map_ne_zero_iff _ (IsFractionRing.injective R[X] K)).mpr (Monic.ne_zero hg)
-    field_simp2
+    simp [field]
     -- Porting note: `norm_cast` was here, but does nothing.
     rw [add_comm, ← map_mul, ← map_add, modByMonic_add_div f hg]
 
